8b4a85457de1f9bccb6360cba503a1f497f75a33
Jason Mace <jmace01@gmail.com>
2014-04-28 19:07:35 -0600

    Adding template files

---
+++ template.php
@@ -0,0 +1,23 @@
+<!doctype html>
+    <html lang="en">
+<head>
+    
+    <!--
+    
+        Copyright 2014 by Jason Mace
+    
+    -->
+  
+    <title>Jason Mace : Hello</title>  
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0">
+    <link rel="stylesheet" href="template.css" type="text/css" />
+</head>
+<body>
+    
+    <header>
+        
+    </header>
+    
+</body>
+    </html>
