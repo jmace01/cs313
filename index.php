6be851174248e56a777957621a9989ba6a027bc1
Jason Mace <jmace01@gmail.com>
2014-04-28 19:50:51 -0600

    Final version for the day

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 $title = 'About Jason';
 
 $body = <<<HTML
-    <img src="jason_above.jpg" />
+    <img src="Jason_above.jpg" />
     <h2>Introducing Jason Mace</h2>
     <p>Hello world! I'm Jason, a computer science major from Houston, Texas.</p>
     <p>I'm enjoy Tae Kwon Do, playing the ukulele, and long walks on the beach.</p>
