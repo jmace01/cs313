8ce3e72df05b6f005caded63a4040cf27eeab2c7
Jason Mace <jmace01@gmail.com>
2014-05-08 16:48:38 -0600

    Updated the design and a preview of the survey

--- survey.php
+++ survey.php
@@ -1,23 +1,74 @@
 <?php
 
-$file = fopen('./survey.txt', 'r');
-
-//fwrite($file, "Google | www.google.com\nBYU-Idaho | www.byui.edu\nESPN | www.espn.com");
-
-$links = '';
-while($line = fgets($file)) {
-    $parts = explode(' | ', $line);
-    $links .= '<a href="http://'.$parts[1].'">'.$parts[0].'</a><br />';
+function getArray($line) {
+    $arr = array();
+    $data = explode('|',$line);
+    foreach ($data as $d) {
+        $d = explode(':', $d);
+        $arr[$d[0]] = $d[1];
+    }
+    return $arr;
 }
 
-fclose($file);
+
+
+function getLine($array) {
+    $items = array();
+    foreach ($array as $key => $item) {
+        $items[] = "$key:$item";
+    }
+    return implode('|',$items);
+}
+
+
+
+if (isset($_POST['submit'])) {
+    $file = fopen('./survey.txt', 'r+');
+    
+    $os   = getArray(fgets($file));
+    $type = getArray(fgets($file));
+    $time = getArray(fgets($file));
+    $spam = getArray(fgets($file));
+    
+    //Validate answers and add them to the results
+    if (key_exists($_POST['os'], $os)) {
+        $os[$_POST['os']]++;
+    }
+    
+    if (key_exists($_POST['type'], $type)) {
+        $type[$_POST['type']]++;
+    }
+    
+    if (key_exists($_POST['time'], $time)) {
+        $time[$_POST['time']]++;
+    }
+    
+    if (isset($_POST['spam'])) {
+        $spam['yes']++;
+    }
+    
+    file_put_contents("survey.txt", "");
+    $output  = getLine($os);
+    $output .= getLine($type);
+    $output .= getLine($time);
+    $output .= getLine($spam);
+
+    
+    fwrite($file, $output);
+    
+    fclose($file);
+    
+    header("Location: survey_results.php");
+    die();
+}
+
 
 
 $title = 'Survey';
 
 $body = <<<HTML
     <h2>Take the survey!</h2>
-    <p>Or go dierectly to <a href="">the results.</a></p>
+    <p>Or go dierectly to <a href="./survey_results.php">the results.</a></p>
     <p><a href="./assignments.php">&lt; Back to assignments</a></p>
     <form method="post" action="">
         <p>Please answer the following questions with your preferences so that we can better market you with our campus spam emails.</p>
@@ -25,30 +76,30 @@ $body = <<<HTML
         <fieldset>
             <legend>Operating System:</legend>
             <p>
-                <input type="radio" name="os" id="mac" value="mac" /><label for="mac">Mac OS</label>
-                <input type="radio" name="os" id="linux" value="linux" /><label for="linux">Linux</label>
-                <input type="radio" name="os" id="win" value="windows" /><label for="win">Windows</label>
-                <input type="radio" name="os" id="other" value="other" /><label for="other">Other</label>
+                <input type="radio" name="os" id="mac" value="Mac OS" /><label for="mac">Mac OS</label>
+                <input type="radio" name="os" id="linux" value="Linux" /><label for="linux">Linux</label>
+                <input type="radio" name="os" id="windows" value="Windows" /><label for="win">Windows</label>
+                <input type="radio" name="os" id="other" value="Other" /><label for="other">Other</label>
             </p>
         </fieldset>
         
         <fieldset>
             <legend>Computer Type:</legend>
             <p>
-                <input type="radio" name="type" id="laptop" value="laptop" /><label for="laptop">Laptop</label>
-                <input type="radio" name="type" id="desktop" value="desktop" /><label for="desktop">Desktop</label>
-                <input type="radio" name="type" id="other" value="other" /><label for="other">Other</label>
+                <input type="radio" name="type" id="laptop" value="Laptop" /><label for="laptop">Laptop</label>
+                <input type="radio" name="type" id="desktop" value="Desktop" /><label for="desktop">Desktop</label>
+                <input type="radio" name="type" id="other" value="Other" /><label for="other">Other</label>
             </p>
         </fieldset>
         
         <fieldset>
             <legend>You spend most of your time on your computer:</legend>
             <p>
-                <input type="radio" name="time" id="gaming" value="gaming" /><label for="gaming">Gaming</label>
-                <input type="radio" name="time" id="working" value="working" /><label for="working">Working</label>
-                <input type="radio" name="time" id="homework" value="homework" /><label for="homework">Doing Homework</label>
-                <input type="radio" name="time" id="cats" value="cats" /><label for="cats">Looking at pictures of cats</label>
-                <input type="radio" name="time" id="other" value="other" /><label for="other">Other</label>
+                <input type="radio" name="time" id="gaming" value="Gaming" /><label for="gaming">Gaming</label>
+                <input type="radio" name="time" id="working" value="Working" /><label for="working">Working</label>
+                <input type="radio" name="time" id="homework" value="Doing Homework" /><label for="homework">Doing Homework</label>
+                <input type="radio" name="time" id="cats" value="Looking at pictures of cats" /><label for="cats">Looking at pictures of cats</label>
+                <input type="radio" name="time" id="other" value="Other" /><label for="other">Other</label>
             </p>
         </fieldset>
         
---
+++ survey_results.php
@@ -0,0 +1,59 @@
+<?php
+
+function getArray($line) {
+    $arr = array();
+    $data = explode('|',$line);
+    foreach ($data as $d) {
+        $d = explode(':', $d);
+        $arr[$d[0]] = $d[1];
+    }
+    return $arr;
+}
+
+
+
+function getQuestion($title, $array) {
+    //Question Header
+    $results = '<h3>'.$title.'</h3>';
+    //Total Submitted
+    $total = 0;
+    foreach ($array as $val) $total += $val;
+    //Results
+    foreach ($array as $key => $value) {
+        $percent = ($value * 100 / $total);
+        $results .= "<div style=\"position: relative; margin-top: 10px;\"><div style=\"width: $percent%; height: 40px; background: lavender;\"><p style=\"position: absolute; left: 10px; top: 0px;\"><b>$key</b> ($value vote or $percent%)</p></div></div>";
+    }
+    return $results;
+}
+
+
+
+$file = fopen('./survey.txt', 'r+');
+
+$os   = getArray(fgets($file));
+$type = getArray(fgets($file));
+$time = getArray(fgets($file));
+$spam = getArray(fgets($file));
+
+
+fclose($file);
+
+
+$results  = getQuestion('Operating System', $os);
+$results .= getQuestion('Computer Type', $type);
+$results .= getQuestion('Time Spent', $time);
+$results .= getQuestion('Spam', $spam);
+
+
+$title = 'Survey Results';
+
+$body = <<<HTML
+    <h2>Survey Results!</h2>
+    <p><a href="./assignments.php">&lt; Back to assignments</a></p>
+    $results
+HTML;
+
+
+include 'template.php';
+
+?>
