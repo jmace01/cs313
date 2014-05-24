c784f44ff63f6336b2fd9a084000ef70e678f261
Jason Mace <jmace01@gmail.com>
2014-05-24 17:38:01 -0600

    More on iDate. Pages progressing more.

--- idate/login.php
+++ idate/login.php
@@ -13,7 +13,7 @@ if (isset($_POST['email'])) {
     if ($statement->rowCount() > 0) {
         $row = $statement->fetch(PDO::FETCH_ASSOC);
         $_SESSION['userID'] = $row['userID'];
-        $ref = isset($_GET['ref']) ? $_GET['ref'] : './';
+        $ref = isset($_GET['ref']) ? $_GET['ref'] : './index.php';
         header("Location: $ref");
     } else {
         header("Location: ./login.php?wrong=1");
@@ -22,7 +22,7 @@ if (isset($_POST['email'])) {
 }
 
 $title = 'Log In';
-$ref = isset($_GET['ref']) ? $_GET['ref'] : '';
+$ref = isset($_GET['ref']) ? '?ref='.$_GET['ref'] : '';
 
 if (isset($_SESSION['userID'])) {
     $content = '<p>You are already logged in!</p>';
@@ -30,7 +30,7 @@ if (isset($_SESSION['userID'])) {
     $error = (isset($_GET['wrong'])) ? '<h3>Invalid Email/Password combination</h3>' : '';
     $content = <<<HTML
     $error
-    <form action="login.php?ref=$ref" method="post">
+    <form action="login.php$ref" method="post">
         <p><label for="email">Email Address:</label></p>
         <p><input type="email" name="email" id="email" style="width: 200px;" /></p>
         <p><label for="password">Password:</label></p>
